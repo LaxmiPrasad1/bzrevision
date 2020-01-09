962c5d28f9ed350c4e2972238d0fd5d6f7e747e7
Lakshmiprasad <laksshmiprasadh55@gmail.com>
2020-01-10 00:07:58 +0530

    checking..

---
+++ src/app/country/country.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-country',
+  templateUrl: './country.component.html',
+  styleUrls: ['./country.component.css']
+})
+export class CountryComponent implements OnInit {
+   c_url="http://restcountries.eu/rest/v2/all"
+   data={}
+  constructor(private ds: DataService) { 
+
+  }
+  ngOnInit()
+  {
+    this.ds.getData(this.c_url).subscribe(d => {
+      this.data = d;
+      console.log(this.data);
+    })
+  }
+}
